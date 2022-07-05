316afe04be69eaf7f8333369fdc0717b3c9527a9
halyna-ala <dawihalyna@gmail.com>
2022-07-05 10:33:52 +0200

    json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import { Profile } from "components/Profile/Profile";
+import {User} from "components/Profile/Profile";
 
 
 export const App = () => {
 
   return (
     <div>
-      <Profile />
+      <Profile 
+      username={User.username}
+      tag={User.tag}
+      location={User.location}
+      avatar={User.avatar}
+      stats={User.stats}/>
     </div>
   );
 };
